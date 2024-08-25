04befd9884f2af8ce7ad5bf09fb29759e6b81836
Achyut Tiwari <64997420+theachyuttiwari@users.noreply.github.com>
2024-08-25 17:39:08 +0530

    Update app.py

--- code/website/app.py
+++ code/website/app.py
@@ -31,7 +31,8 @@ def main():
             'vts': [vts]
         })
         st.write("Shape of user_input:", user_input.shape)  # Print the shape of user_input
-        prediction = model.predict(user_input)
+        #prediction = model.predict(user_input)
+        prediction = model.predict(user_input.values)
         prediction_proba = model.predict_proba(user_input)
 
         if prediction[0] == 1:
